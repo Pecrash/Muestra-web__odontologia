2c08f289f23e762b88d41a2767b1d81a497e302e
Junior <guitar.sound12@gmail.com>
2023-03-06 11:04:36 -0500

    enlace a redes

--- components/footer.php
+++ components/footer.php
@@ -1,29 +1,35 @@
 <footer class="secFooter">
-        <img src="../assets/IconDental.png" alt="" class="secFooter__logo">
-        <section class="secFooter__redes">
-            <h2 class="secFooter__redes-tittle">Redes Sociales</h2>
-            <section class="secFooter__redes-iconos">
+    <img src="../assets/IconDental.png" alt="" class="secFooter__logo">
+    <section class="secFooter__redes">
+        <h2 class="secFooter__redes-tittle">Redes Sociales</h2>
+        <section class="secFooter__redes-iconos">
+            <a href="https://www.facebook.com/profile.php?id=100090796002084">
                 <img src="../assets/Redes/facebook.svg" alt="">
+            </a>
+            <a href="https://www.instagram.com/pecrash_dev/?igshid=YmMyMTA2M2Y%3D">
                 <img src="../assets/Redes/instagram.svg" alt="">
+            </a>
+            <a href="">
                 <img src="../assets/Redes/twitter.svg" alt="">
-            </section>
+            </a>
         </section>
-        <section class="secFooter__nav">
-            <a href="#">Nosotros</a>
-            <a href="#">Especialidades</a>
-            <a href="#">Nuestro Equipo</a>
-            <a href="#">Entérate</a>
-            <a href="#">Contáctanos</a>
-        </section>
-        <section class="secFooter__contacto">
-            <h2 class="secFooter__contacto-tittle">Contacto</h2>
-            <p class="secFooter__contacto-parrafo">
-                pecrash12@gmail.com <br>
-                +57 3044376024
-            </p>
-        </section>
-    </footer>
-    <script src="./src/index.js"></script>
+    </section>
+    <section class="secFooter__nav">
+        <a href="#">Nosotros</a>
+        <a href="#">Especialidades</a>
+        <a href="#">Nuestro Equipo</a>
+        <a href="#">Entérate</a>
+        <a href="#">Contáctanos</a>
+    </section>
+    <section class="secFooter__contacto">
+        <h2 class="secFooter__contacto-tittle">Contacto</h2>
+        <p class="secFooter__contacto-parrafo">
+            pecrash12@gmail.com <br>
+            +57 3044376024
+        </p>
+    </section>
+</footer>
+<script src="./src/index.js"></script>
 </body>
 
 </html>
